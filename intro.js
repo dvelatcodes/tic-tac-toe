3b00e5af9925dc7dabfc28525efb15bca2812c0f
Love Olubummo <olubummolove3@gmail.com>
2023-10-11 19:22:27 +0100

    feature: got access to gameboard and commment element from js file and styled gameboard and body

--- intro.js
+++ intro.js
@@ -1,3 +1,6 @@
-console.log("connected")
-
 // git config--global credential.useHttpPath true   this is used to authenticate a new user using a laptop git was used in
+
+console.log("connected")
+const gameBoard = document.querySelector(".gameboard");
+const comments = document.querySelector(".comments");
+
