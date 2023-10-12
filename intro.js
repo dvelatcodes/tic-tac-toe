1e7f0637090e6022ba4fbcc653cae46c920df9ca
Love Olubummo <olubummolove3@gmail.com>
2023-10-12 05:39:22 +0100

    new changes

--- intro.js
+++ intro.js
@@ -2,13 +2,33 @@
 
 console.log("connected")
 const gameBoard = document.querySelector(".gameboard");
-const comments = document.querySelector(".comments");
+const comment  = document.querySelector(".comments");
 const boxes = ["", "", "", "", "", "", "", "", ""];
+comment.textContent  = "Circle plays first"
+console.log("jj")
 
 (function createBox() {
     boxes.forEach((_box, index) => {
         const square = document.createElement("div");
         square.classList.add("square");
+        square.id = index;
+        square.addEventListener("click", onPress)
         gameBoard.append(square)
     })
 })()
+
+
+
+
+function onPress(e){
+    const content = document.createElement("div");
+    if (comment === "Circle plays first") {
+        content.classList.add("circle")
+        comment.textContent = "cross turn to play"
+    }
+    if (comment === "Cross turn to play") {
+        content.classList.add("cross")
+        comment.textContent = "Circle plays first"
+    }
+    e.target.append(content)
+}
